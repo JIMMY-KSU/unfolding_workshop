df6968e02e11c56cae4f4ad8a3fe7ab95b216996
John Boyington <mjcb@ksu.edu>
2019-03-22 12:22:35 -0500

    cleaning up examples

--- example1.py
+++ example1.py
@@ -4,7 +4,6 @@ from plotter import plotit
 
 
 # load in the data
-group_structure = np.load('data/struct.npy')
 responses = np.load('data/response0.npy')
 errors = np.load('data/error0.npy')
 response_functions = np.load('data/rfs.npy')
@@ -14,12 +13,11 @@ true_solution = np.load('data/ds0.npy')
 default_spectrum = (np.ones(len(true_solution)) / len(true_solution)) * np.sum(true_solution)
 
 
-# unfold with maxed
+# unfold
 params = {'Omega': 8, 'max_iter': 200, 'tol': 1E-4}
 maxed_solution = unfold(responses, errors, response_functions, default_spectrum, method='MAXED', params=params)
 gravel_solution = unfold(responses, errors, response_functions, default_spectrum, method='Gravel', params=params)
 
 # plot the solutions
 plotit(default_spectrum, maxed_solution, true_solution, 'MAXED')
-
 plotit(default_spectrum, gravel_solution, true_solution, 'Gravel')
--- example2.py
+++ example2.py
@@ -4,7 +4,6 @@ from plotter import plotit
 
 
 # load in the data
-group_structure = np.load('data/struct.npy')
 responses = np.load('data/response0.npy')
 errors = np.load('data/error0.npy')
 response_functions = np.load('data/rfs.npy')
@@ -13,12 +12,11 @@ true_solution = np.load('data/ds0.npy')
 # default spectrum off by value of one half
 default_spectrum = np.load('data/ds0.npy') / 2
 
-# unfold with maxed
+# unfold
 params = {'Omega': 8, 'max_iter': 200, 'tol': 1E-4}
 maxed_solution = unfold(responses, errors, response_functions, default_spectrum, method='MAXED', params=params)
 gravel_solution = unfold(responses, errors, response_functions, default_spectrum, method='Gravel', params=params)
 
 # plot the solutions
 plotit(default_spectrum, maxed_solution, true_solution, 'MAXED')
-
 plotit(default_spectrum, gravel_solution, true_solution, 'Gravel')
